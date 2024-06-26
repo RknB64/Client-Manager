812c4bbf2adafcf5b2b375481193ac3bea9cd761
RknB64 <ercan.balikel86@gmail.com>
2024-06-26 16:23:25 +0200

    Little bit of styling

--- src/main/java/cda/clientmanager/App.java
+++ src/main/java/cda/clientmanager/App.java
@@ -21,32 +21,6 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        System.out.println("JavaFX version: " + System.getProperty("javafx.version"));
-        ClientDAO clientDAO = ClientDAO.getInstance();
-//        List<Client> clientList = clientDAO.getClients();
-//        System.out.println(clientList);
-//
-//        Client c1 = new Client("Jon", "JohnnyJon", "0123456789", "ici", 56000, "Vannes");
-//        Client c2 = new Client( "Kazama", "Jin", "9876543210", "là-bas", 62000, "Quelque part");
-//        clientList.add(c1);
-//        clientList.add(c2);
-//        clientDAO.saveToFile(clientList);
-//
-//        clientList = clientDAO.getClients();
-//        System.out.println(clientList);
-//
-//        Client clientToDelete = clientDAO.getClientByLastName("Jon");
-//        System.out.println(clientToDelete);
-//
-//        Client clientToUpdate = clientDAO.getClientByFirstName("Jin");
-//        clientToUpdate.setFirstName("Asuka");
-//        System.out.println(clientToUpdate);
-//
-//        clientDAO.updateClient(clientToUpdate);
-//
-//        clientDAO.deleteClient(clientToDelete);
-        System.out.println(clientDAO.getClients());
-
         launch();
     }
 }
